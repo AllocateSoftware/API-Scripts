2d13be9220e02eabf7214292cc5ce3ccd545c110
adams <a4szczepaniak@gmail.com>
2020-06-26 17:00:02 +0100

    EligibleWorkersAPI probe
    Signed-off-by: adams <a4szczepaniak@gmail.com>

---
+++ public-scripts/EligibleWorkersAPI/getEligibleWorkers.py
@@ -0,0 +1,37 @@
+import sys
+sys.path.append("../Util")
+import testUtil
+import http.client
+import json
+import requests
+
+CLIENT_ID = "9E5aqUf1aW5toUHDZlQWETcoJLZOWYMT"
+CLIENT_SECRET = "k-Pp7FbN_ikTYhv6YSMXYHzeJ4nD9lzmroMtnNG216xGb58N35OuwGP_S9AZ5XHP"
+AUDIENCE = "http://healthsuite.allocatesoftware.com/api/v1/eligibleworkersproxyapi"
+
+API_ADDRESS="https://interop-eligibleworkers-gateway.allocate-dev.co.uk/api/v1/eligibleworkers"
+
+# Get access token for the API
+tokenQuery = testUtil.getTokenQuery(
+    clientId=CLIENT_ID, clientSecret=CLIENT_SECRET, audience=AUDIENCE)
+token = testUtil.getToken(tokenQuery)
+
+bearer_auth = token["access_token"]
+bearer = "bearer {}".format(bearer_auth)
+print("Token: %s" % bearer)
+
+# Generate randon proposal id
+customerCode = "EPSON"
+
+
+# Call the API
+srvEndpoint = "{}?customerCode={}".format(API_ADDRESS, customerCode)
+print("URI: %s" % srvEndpoint)
+
+
+r = requests.get(url=srvEndpoint, headers={
+                 "accept": "application/json", "authorization": bearer}, verify=True)
+rsp = r.text
+
+print("Response Code: %s" % r.status_code)
+print("Response: %s" % rsp)
